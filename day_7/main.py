87c4df99e5948eef00ce86931b899c06a40d1089
addisonkline <77369109+addisonkline@users.noreply.github.com>
2025-12-08 18:22:33 -0500

    i'll come back to day 7 part 2 later

--- day_7/main.py
+++ day_7/main.py
@@ -25,10 +25,10 @@ class TachyonManifold:
     
     def run_beam(
         self,
-    ) -> tuple[list[list[str]], int]:
+    ) -> tuple[list[list[str]], int, int]:
         """
         Run the beam from the given manifold diagram.
-        Returns `diagram_with_beam, final_num_beams`.
+        Returns `diagram_with_beam, final_num_beams, final_num_timelines`.
         """
         diagram_curr = self.diagram
 
@@ -36,6 +36,7 @@ class TachyonManifold:
         cols = len(line_first)
 
         num_splits = 0
+        num_timelines = 0
 
         for i in range(1, len(self.diagram)):
             line_prev = diagram_curr[i - 1]
@@ -53,6 +54,7 @@ class TachyonManifold:
                             diagram_curr[i][j - 1] = "|"
                             diagram_curr[i][j + 1] = "|"
                             num_splits += 1
+                            num_timelines += 2
                         else:
                             print(f"beam continuing down")
                             diagram_curr[i][j] = "|"
@@ -62,18 +64,54 @@ class TachyonManifold:
                         pass
                     case _:
                         raise ValueError(f"illegal character in diagram: {char}")
+                    
+        for i in range(1, len(self.diagram)):
+            for j in range(1, cols - 1):
+                if (diagram_curr[i][j - 1] == "^") and (diagram_curr[i][j] == "|") and (diagram_curr[i][j + 1] == "^"):
+                    num_timelines -= 1
 
-        return diagram_curr, num_splits
+        return diagram_curr, num_splits, num_timelines
+    
+    def count_timelines(
+        self,
+    ) -> int:
+        """
+        Determine the number of possible timelines for this configuration.
+        """
+        raise NotImplementedError
+    
+    def _timelines_in_line(
+        self,
+        line: list[str]
+    ) -> int:
+        """
+        Count the number of timelines in this row.
+        """
+        line_result = line
+
+        for i in range(0, len(line)):
+            if i == 0:
+                if line[i + 1] == "^":
+                    line_result[i] = "|"
+                continue
+            if i == len(line) - 1:
+                if line[i - 1] == "^":
+                    line_result[i] = "|"
+                continue
+            if (line[i - 1] == "^") or (line[i + 1] == "^"):
+                line_result[i] = "|"
+        
+        return line_result.count("|")
 
 
 if __name__ == "__main__":
-    filepath = "day_7/input.txt"
+    filepath = "day_7/input_test.txt"
     with open(filepath) as file:
         contents = file.read()
         tm = TachyonManifold.from_string(contents)
-        diagram, count = tm.run_beam()
+        diagram, num_splits, num_timelines = tm.run_beam()
         print("=" * 80)
         for line in diagram:
             print(line)
         print("-" * 80)
-        print(f"final number of splits: {count}")
+        print(f"final number of timelines: {num_timelines}")
